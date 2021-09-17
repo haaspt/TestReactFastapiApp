bcb484f4c01258645f68f95edb70c2efdd3075c8
Tyler Haas <tyler.haas@getcruise.com>
2021-09-17 10:57:37 -0700

    Fetch initial data from backend

--- services/frontend/src/App.js
+++ services/frontend/src/App.js
@@ -1,26 +1,22 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import Header from './components/Header';
 import TaskList from './components/TaskList';
 
 const App = () => {
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: "Kiss Ally!",
-      date: "9/16/2021"
-    },
-    {
-      id: 2,
-      text: "Make steak",
-      date: "9/17/2021"
-    },
-    {
-      id: 3,
-      text: "Do work",
-      date: "Never!"
-    },
-  ])
+  const [tasks, setTasks] = useState([])
 
+  useEffect(() => {
+    const fetchInitialTasks = async () => {
+      const result = await axios(
+        'http://localhost:8000/tasks',
+      );
+
+      setTasks(result.data);
+    };
+
+    fetchInitialTasks();
+  }, []);
   const addTask = (taskText, taskDate) => {
     
     const newTask = {
