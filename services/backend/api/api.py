e5d5a308a986793f73e17ee3fda464da8b10d159
Tyler Haas <tyler.haas@getcruise.com>
2021-09-17 10:56:59 -0700

    Enable cors

--- services/backend/api/api.py
+++ services/backend/api/api.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 from typing import List
 from . import db
@@ -9,6 +10,22 @@ from .types import (
 )
 
 app = FastAPI()
+
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "localhost:3000"
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
