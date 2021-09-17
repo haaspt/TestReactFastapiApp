b5eef37b03de2b1e48394487bf8eee3205cfff73
Tyler Haas <patrick.tyler.haas@gmail.com>
2021-09-16 21:45:21 -0700

    Add propTypes and optional date render

--- services/frontend/src/components/Task.js
+++ services/frontend/src/components/Task.js
@@ -1,13 +1,24 @@
+import { PropTypes } from 'prop-types';
+
 const Task = ({ task, onTaskDelete }) => {
     return (
         <div>
             <p>{task.text}</p>
-            <p>{task.date}</p>
+            {task.date && <p>{task.date}</p>}
             <button type="button" onClick={() => onTaskDelete(task.id)}>
                 Remove
             </button>
         </div>
     )
+}
+
+Task.propTypes = {
+    task: PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        text: PropTypes.string.isRequired,
+        date: PropTypes.string,
+    }),
+    onTaskDelete: PropTypes.func,
 }
 
 export default Task;
